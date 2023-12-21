5189c59f14afa1b177e142b2b47a487fb409f156
enikebraimoh <richardbraimoh@gmail.com>
2023-12-21 08:00:02 +0100

    Initial Commit

---
+++ Nuraz/NurazApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurazApp.swift
+//  Nuraz
+//
+//  Created by Enike Braimoh on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct NurazApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
